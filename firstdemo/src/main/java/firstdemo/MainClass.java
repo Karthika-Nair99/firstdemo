da85ccb229ee33ab5b9a48a9ae03d7c0321ad8e5
Karthika-Nair99 <karthikavnair99@gmail.com>
2021-08-16 13:54:53 +0530

    First Commit

---
+++ firstdemo/src/main/java/firstdemo/MainClass.java
@@ -0,0 +1,11 @@
+package firstdemo;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+		System.out.println("GA");
+		System.out.println("Have a good day");
+		System.out.println("Welcome to Git");
+	}
+
+}
