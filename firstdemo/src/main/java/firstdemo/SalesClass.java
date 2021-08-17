65288bdb3dee42ec16b687a8270108cfd8de813d
Karthika-Nair99 <karthikavnair99@gmail.com>
2021-08-17 11:38:38 +0530

    sum added

--- firstdemo/src/main/java/firstdemo/SalesClass.java
+++ firstdemo/src/main/java/firstdemo/SalesClass.java
@@ -3,10 +3,13 @@ package firstdemo;
 public class SalesClass {
 	public void display() {
 		int[] arr= {23,24,25};
+		int sum=0;
 		for(int num:arr) {
+			sum+=1;
 			System.out.println(num);
 		}
-	}	
+	    System.out.println(sum);
+	}  
 	public void print() {
 		System.out.println("Print Array");
 		display();
